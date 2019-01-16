fd70871c22c52c5ba65265195b4cc58464b54f62
Manny Orozco <orozco89@gmail.com>
2019-01-16 12:05:59 -0800

    added mongoose

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
 
   // call next because you want to continue to next method
@@ -22,7 +22,7 @@ app.post('/api/posts', (req, res, next) => {
   });
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     { id: '1', title: 'First server-side post', content: 'This is coming from the server' },
     { id: '2', title: 'Second server-side post', content: 'This is coming from the server!' }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -7,12 +7,12 @@ import { HttpClient } from '@angular/common/http';
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
-  private url = 'http://localhost:3000/api';
+  private url = 'http://localhost:3000/api/posts';
 
   constructor(private http: HttpClient) {}
 
   public getPosts() {
-    this.http.get<{ message: string; posts: Post[] }>(`${this.url}/posts`).subscribe(postData => {
+    this.http.get<{ message: string; posts: Post[] }>(this.url).subscribe(postData => {
       this.posts = postData.posts;
 
       // ... created copy of array
@@ -26,9 +26,11 @@ export class PostsService {
       title: title,
       content: content
     };
-
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http.post<{ message: string }>(this.url, post).subscribe(responseData => {
+      console.log(responseData.message);
+      this.posts.push(post);
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
   public getPostUpdateListener(): Observable<Post[]> {
