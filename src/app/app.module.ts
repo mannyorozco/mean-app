146d3555181c7d817e4bb31a9461ec8dc289fc18
Manny Orozco <orozco89@gmail.com>
2019-01-16 09:21:17 -0800

    added post and mock get to api.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  MatInputModule,
-  MatCardModule,
-  MatButtonModule,
-  MatToolbarModule,
-  MatExpansionModule
-} from '@angular/material';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, PostCreateComponent, PostListComponent],
@@ -21,6 +16,7 @@ import { PostListComponent } from './posts/post-list/post-list.component';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatInputModule,
     MatCardModule,
     MatButtonModule,
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
 import { Subject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
+  private url = 'http://localhost:3000/api';
 
-  public getPosts(): Post[] {
-    // spread oporator copies the posts array without modifying original data.
-    return [...this.posts];
+  constructor(private http: HttpClient) {}
+
+  public getPosts() {
+    this.http.get<{ message: string; posts: Post[] }>(`${this.url}/posts`).subscribe(postData => {
+      this.posts = postData.posts;
+
+      // ... created copy of array
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
   public addPost(title: string, content: string): void {
     const post: Post = {
+      id: '',
       title: title,
       content: content
     };
