a87c4895fedd03e8e71bac78e7a109ba507b86e7
Manny Orozco <orozco89@gmail.com>
2019-05-05 21:33:03 -0700

    finished and deployed

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // allow request to images folder
-app.use('/images', express.static(path.join('backend/images')));
+app.use('/images', express.static(path.join('images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://nodeangular-env.ph6b6ytap9.us-east-2.elasticbeanstalk.com/api'
 };
